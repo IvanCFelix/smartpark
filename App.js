189800c6b6a521e71de866fb6297fed1388acb5b
Ivan Cota Felix <gcota@emt.dev>
2022-11-08 16:32:40 -0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Pressable, StyleSheet } from "react-native";
+import HomeScreen from "./src/Screens/Home";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Inicio"
+          component={HomeScreen}
+          options={{
+            headerRight: () => (
+              <Pressable
+                onPress={() => alert("This is a button!")}
+                style={styles.toggleButton}
+                title="Info"
+              >
+                <Icon name="qrcode" size={25} color="#900" />
+              </Pressable>
+            ),
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  toggleButton: {
+    display: "flex",
+    height: 40,
+    width: 40,
+    borderRadius: 40,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
