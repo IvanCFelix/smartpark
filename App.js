4ea0e17ef5f5d3b7446c8e74394069ef62d6b900
Ivan Cota Felix <gcota@emt.dev>
2022-11-21 17:20:17 -0700

    drawer navigation terminada

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Pressable, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import HomeScreen from "./src/Screens/HomeScreen";
-import Icon from "react-native-vector-icons/FontAwesome";
 import QrModal from "./src/Components/QrModal";
 import { createContext, useState } from "react";
+import "react-native-gesture-handler";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 export const userContext = createContext();
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
+  const Drawer = createDrawerNavigator();
+
   const [userInfo, setUserInfo] = useState(null);
   return (
     <userContext.Provider
       value={{ userInfo: userInfo, setUserInfo: setUserInfo }}
     >
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Inicio" component={HomeScreen} />
-          <Stack.Group
+        <Drawer.Navigator>
+          <Drawer.Screen name="Inicio" component={HomeScreen} />
+          <Drawer.Group
             screenOptions={{ presentation: "modal", headerShown: false }}
           >
-            <Stack.Screen name="QrModal" component={QrModal} />
-          </Stack.Group>
-        </Stack.Navigator>
+            <Drawer.Screen name="Escanear QR" component={QrModal} />
+          </Drawer.Group>
+        </Drawer.Navigator>
       </NavigationContainer>
     </userContext.Provider>
   );
